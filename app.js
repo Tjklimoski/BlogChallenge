10c43ffcadb017ca0963066c1ead865bfc2bc883
TJ Klimoski <tjklimoski@gmail.com>
2023-01-31 17:16:30 -0500

    Add default content for pages

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const port = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+const homeStartingContent = 'This is starting content for home';
+const aboutContent = 'This is content for about';
+const contactContent = 'This is content for the contact page'
+
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
